72d911a075af8a5568ecf98aa3a08c1930635e87
Bhagyashree <bhagyashree9402@gmail.com>
2020-06-05 21:58:04 -0700

    started with new version of readme

---
+++ index.js
@@ -0,0 +1,92 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+inquirer.prompt([{
+    type: "input",
+    name: "project_title",
+    message: "What is the project title?",
+},
+
+{
+    type: "input",
+    name: "description",
+    message: "Write the brief description of your project",
+},
+
+{
+    type: "input",
+    name: "installaton",
+    message: "What command should be run to install dependencies?",
+},
+
+{
+    type: "input",
+    name: "usage",
+    message: "Describe the usage of your project",
+},
+
+{
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "Apache", "GPL", "Mozilla", "none"],
+},
+
+{
+    type: "input",
+    name: "contribution",
+    message: "What does the user need to know about contributing to the repo?"
+},
+
+{
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+},
+
+{
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username",
+},
+
+{
+    type: "input",
+    name: "email",
+    message: "Enter your Email",
+},
+]).then(function (response) {
+    console.log(response);
+    //fs.unlinkSync("readme.md");
+    fs.writeFile("readme.md", "# " + response.project_title + "\n", function (error) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log("success");
+        }
+    })
+
+    fs.appendFile("readme.md", "\n" + "## Description" + "\n" + response.description + "\n"
+        + "\n" + "## Table of Contents" + "\n" + "- [Installation][## Installation  ]"
+        + "\n" + "- [Usage][" + response.usage + "]" + "\n" + "- [License][" + response.license + "]"
+        + "\n" + "- [Contribution][" + response.contribution + "]" + "\n" + "- [Tests][" + response.test + "]" + "\n"
+        + "\n" + "## Installation" + "\n" + response.installaton + "\n"
+        + "\n" + "## Usage" + "\n" + response.usage + "\n"
+        + "\n" + "## License" + "\n" + response.license + "\n"
+        + "\n" + "## Contribution" + "\n" + response.contribution + "\n"
+        + "\n" + "## Tests" + "\n" + response.test + "\n"
+        + "\n" + "## Questions" + "\n" + "### Link to GitHub page: " + "![alt text](https://github.com/" + response.username + ")"
+        + "\n" + "### Email: " + response.email + "\n",
+        function (error) {
+            if (error) {
+                console.log(error);
+
+            }
+            else {
+                console.log("success");
+
+            }
+        })
+
+})
---
+++ readme.md
@@ -0,0 +1,30 @@
+# ncnnc
+
+## Description
+vnvnvn
+
+## Table of Contents
+- [Installation][## Installation  ]
+- [Usage][nchfc]
+- [License][Apache]
+- [Contribution][jcnvn]
+- [Tests][djdjd]
+
+## Installation
+cnvnv
+
+## Usage
+nchfc
+
+## License
+Apache
+
+## Contribution
+jcnvn
+
+## Tests
+djdjd
+
+## Questions
+### Link to GitHub page: ![alt text](https://github.com/Bhagyashree9402)
+### Email: bhagyashree9402@gmail.com
